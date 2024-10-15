5a940df62e9d408668fe78ef97ff6d622178a831
EthanMorin <119540016+EthanMorin@users.noreply.github.com>
2024-10-15 10:12:51 -0600

    update

--- nuxtui/app/app.vue
+++ nuxtui/app/app.vue
@@ -1,5 +1,12 @@
+<script setup lang="ts">
+import { defineAsyncComponent } from 'vue';
+
+const NavBar = defineAsyncComponent(() => import('../components/NavBar.vue'));
+</script>
+
 <template>
   <NuxtRouteAnnouncer />
   <NuxtLoadingIndicator />
+  <NavBar />
   <NuxtPage />
 </template>
---
+++ nuxtui/components/NavBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav>
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/">About</router-link></li>
+      <li><router-link to="/">Contact</router-link></li>
+    </ul>
+  </nav>
+</template>
