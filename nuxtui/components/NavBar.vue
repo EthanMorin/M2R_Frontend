461a89d05ecf54e603809a4dfd44992f1b0ace3e
EthanMorin <119540016+EthanMorin@users.noreply.github.com>
2024-10-17 20:15:55 -0600

    update nav styles

--- nuxtui/components/NavBar.vue
+++ nuxtui/components/NavBar.vue
@@ -1,9 +1,21 @@
 <template>
-  <nav>
-    <ul>
-      <li><router-link to="/">Home</router-link></li>
-      <li><router-link to="/">About</router-link></li>
-      <li><router-link to="/">Contact</router-link></li>
+  <nav class="bg-gray-800 p-4">
+    <ul class="flex space-x-4">
+      <li>
+        <router-link to="/" class="text-white hover:text-gray-400">
+          Home
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/" class="text-white hover:text-gray-400">
+          About
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/" class="text-white hover:text-gray-400">
+          Contact
+        </router-link>
+      </li>
     </ul>
   </nav>
 </template>
