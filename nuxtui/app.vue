392f4751567e8afe6904864145b4633604a01926
EthanMorin <119540016+EthanMorin@users.noreply.github.com>
2024-10-23 12:46:29 -0600

    update file structre

---
+++ nuxtui/app.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+	// import { defineAsyncComponent } from 'vue';
+</script>
+
+<template>
+	<NuxtRouteAnnouncer />
+	<NuxtLoadingIndicator />
+	<NavBar />
+	
+</template>
---
+++ nuxtui/layouts/default.vue
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import FooterBar from "../components/FooterBar.vue";
+	import NavBar from "../components/NavBar.vue";
+</script>
+<template>
+	<NavBar />
+  <slot/>
+	<FooterBar />
+</template>
